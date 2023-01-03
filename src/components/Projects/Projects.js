e25918458e2de6cedf83109cab319f31565572c9
OLAGUNJU ISAAC <isaac.olagunju@decagon.dev>
2023-01-03 12:57:36 +0100

    My Portfolio

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { projects } from '../../constants/constants';
+import { inProgress, projects } from '../../constants/constants';
 
 const Projects = () => (
   <Section nopadding id="projects">
@@ -12,7 +12,7 @@ const Projects = () => (
       {projects.map((p, i) => {
         return (
           <BlogCard key={i}>
-          <Img src={p.image} />
+            <Img src={p.image} />
             <TitleContent>
               <HeaderThree>{p.title}</HeaderThree>
               <Hr />
@@ -30,6 +30,34 @@ const Projects = () => (
               <ExternalLinks href={p.visit}>View Project</ExternalLinks>
               {/* <ExternalLinks href={p.source}>Source</ExternalLinks> */}
             </UtilityList>
+          </BlogCard>
+        );
+      })}
+    </GridContainer>
+    <SectionDivider />
+    <SectionTitle main>Projects in Progress</SectionTitle>
+    <GridContainer>
+      {inProgress.map((p, i) => {
+        return (
+          <BlogCard key={i}>
+            <Img src={p.image} />
+            <TitleContent>
+              <HeaderThree>{p.title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo className="card-info">{p.description}</CardInfo>
+            <div>
+              <TitleContent>Stack</TitleContent>
+              <TagList>
+                {p.tags.map((t, i) => {
+                  return <Tag key={i}>{t}</Tag>;
+                })}
+              </TagList>
+            </div>
+            {/* <UtilityList>
+              <ExternalLinks href={p.visit}>View Project</ExternalLinks>
+              <ExternalLinks href={p.source}>Source</ExternalLinks>
+            </UtilityList> */}
           </BlogCard>
         );
       })}
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,16 +3,50 @@ export const projects = [
     title: "Project GPT",
     description:
       "Using ReactJs, Node.js, Material UI, Animista and Angrytools, i built this project to showcase my web development skills. It is a dark themed, modern UI website that gives information on the ChatGPT Open AI",
-    image: "images/myProjectgptScreenshot.png",
+    image: "/images/myProjectgptScreenshot.png",
     tags: ["React", "MaterialUI", "Node"],
     source: "https://myprojectgpt.onrender.com/",
     visit: "https://myprojectgpt.onrender.com/",
     id: 0,
-  },
+  }
+  // {
+  //   title: "E-Commerce",
+  //   description:
+  //     "While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+  //   image: "/images/2.png",
+  //   tags: ["React", "JavaScript"],
+  //   source: "https://google.com",
+  //   visit: "https://google.com",
+  //   id: 1,
+  // }
+  // {
+  //   title: "WebRTC App",
+  //   description:
+  //     "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+  //   image: "/images/3.jpg",
+  //   tags: ["React", "WebRTC"],
+  //   source: "https://google.com",
+  //   visit: "https://google.com",
+  //   id: 2,
+  // },
+  // {
+  //   title: "Unichat",
+  //   description:
+  //     "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+  //   image: "/images/4.jpg",
+  //   tags: ["React", "ChatEngine", "Firebase"],
+  //   source: "https://google.com",
+  //   visit: "https://google.com",
+  //   id: 3,
+  // },
+];
+
+export const inProgress = [
+  
   {
     title: "E-Commerce",
     description:
-      "While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+      "In this project, i am using advanced React & JavaScript topics and i will be integrating Stripe for card transactions.",
     image: "/images/2.png",
     tags: ["React", "JavaScript"],
     source: "https://google.com",
@@ -22,7 +56,7 @@ export const projects = [
   {
     title: "WebRTC App",
     description:
-      "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+      "In this project, i am building a React Video Chat Application using WebRTC.",
     image: "/images/3.jpg",
     tags: ["React", "WebRTC"],
     source: "https://google.com",
@@ -32,7 +66,7 @@ export const projects = [
   {
     title: "Unichat",
     description:
-      "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+      "Here, i will be creating a full Realtime Chat Application",
     image: "/images/4.jpg",
     tags: ["React", "ChatEngine", "Firebase"],
     source: "https://google.com",
